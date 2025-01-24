b2d730dc6b44faaa60170ca6d1e126cc3e6e67c3
Pedro <p.arantes@protonmail.com>
2025-01-24 14:50:55 +0100

    removed unecessary imports

--- src/Components/Color/ColorForm.jsx
+++ src/Components/Color/ColorForm.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import ColorInput from './ColorInput'
-export default function ColorForm({
-    onSubmitColor,
-    initialData
-}) {
-
-    const styles = {
-        form: {
-            display: "flex",
-            flexDirection: "column",
-            maxWidth: "300px",
-            gap: ".5rem"
-        }
-    }
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData);
-        onSubmitColor(data);
-    }
-    return (
-        <form style={styles.form} onSubmit={handleSubmit}>
-            <label htmlFor="role">Role</label>
-            <input type="text"
-                id="role"
-                name="role"
-                defaultValue={initialData.role} />
-            <label htmlFor="hex">Hex</label>
-            <ColorInput id="hex" defaultValue={initialData.hex} />
-            <label htmlFor="contrastText">Contrast Text</label>
-            <ColorInput id="contrastText" defaultValue={initialData.contrastText} />
-            <button type="submit">Add Color</button>
-        </form>
-    )
+import ColorInput from "./ColorInput";
+export default function ColorForm({ onSubmitColor, initialData }) {
+  const styles = {
+    form: {
+      display: "flex",
+      flexDirection: "column",
+      maxWidth: "300px",
+      gap: ".5rem",
+    },
+  };
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onSubmitColor(data);
+  }
+  return (
+    <form style={styles.form} onSubmit={handleSubmit}>
+      <label htmlFor="role">Role</label>
+      <input
+        type="text"
+        id="role"
+        name="role"
+        defaultValue={initialData.role}
+      />
+      <label htmlFor="hex">Hex</label>
+      <ColorInput id="hex" defaultValue={initialData.hex} />
+      <label htmlFor="contrastText">Contrast Text</label>
+      <ColorInput id="contrastText" defaultValue={initialData.contrastText} />
+      <button type="submit">Add Color</button>
+    </form>
+  );
 }
--- src/Components/Color/ContrastChecker.jsx
+++ src/Components/Color/ContrastChecker.jsx
@@ -1,34 +1,50 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export default function ContrastChecker({ hex, contrast }) {
-    const [overallContrast, setContrast] = useState("")
+  const [overallContrast, setContrast] = useState("");
 
-    useEffect(() => {
-        async function startFetching() {
-            const response = await fetch(
-                "https://www.aremycolorsaccessible.com/api/are-they",
-                {
-                    method: "POST",
-                    mode: 'cors',
-                    body: JSON.stringify({ colors: [hex, contrast] }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            let data = await response.json()
-            setContrast(data.overall)
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        "https://www.aremycolorsaccessible.com/api/are-they",
+        {
+          method: "POST",
+          mode: "cors",
+          body: JSON.stringify({ colors: [hex, contrast] }),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        startFetching();
-    }, []);
+      );
+      let data = await response.json();
+      setContrast(data.overall);
+    }
+    startFetching();
+  }, []);
 
-    return (
+  return (
+    <>
+      {overallContrast === "Yup" && (
         <>
-            {overallContrast === "Yup" && <><span style={{ backgroundColor: "green", color: "white" }}>Overall Contrast Score: {overallContrast}</span></>}
-            {overallContrast === "Kinda" && <><span style={{ backgroundColor: "yellow", color: "black" }}>Overall Contrast Score: {overallContrast}</span></>}
-            {overallContrast === "Nope" && <><span style={{ backgroundColor: "red", color: "white" }}>Overall Contrast Score: {overallContrast}</span ></>}
+          <span style={{ backgroundColor: "green", color: "white" }}>
+            Overall Contrast Score: {overallContrast}
+          </span>
         </>
-    )
-
+      )}
+      {overallContrast === "Kinda" && (
+        <>
+          <span style={{ backgroundColor: "yellow", color: "black" }}>
+            Overall Contrast Score: {overallContrast}
+          </span>
+        </>
+      )}
+      {overallContrast === "Nope" && (
+        <>
+          <span style={{ backgroundColor: "red", color: "white" }}>
+            Overall Contrast Score: {overallContrast}
+          </span>
+        </>
+      )}
+    </>
+  );
 }
